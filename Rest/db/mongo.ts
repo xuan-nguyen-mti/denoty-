0256cb6a35a285fdc67da3e8b47d3866c1eda538
Xuan NT <xuan.nguyentruong@one-line.com>
2021-10-20 00:59:36 +0700

    update connectio string

--- Rest/db/mongo.ts
+++ Rest/db/mongo.ts
@@ -3,7 +3,7 @@ import { Bson, MongoClient } from "https://deno.land/x/mongo@v0.26.0/mod.ts";
 const client = new MongoClient();
 
 //Connecting to a Local Database
-await client.connect("mongodb+srv://deno-user:u7RLwgSNwwNXZngw@cluster0.lzoni.mongodb.net/test??authMechanism=SCRAM-SHA-1");
+await client.connect("connection string");
 
 
 
